783c6718bec4ae39271650d5d7a2189d8fe3f00d
Dan Q <danq@automattic.com>
2025-08-30 18:45:41 +0100

    Expanded skeleton framework

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,10 @@
 export default async function(eleventyConfig) {
   eleventyConfig.setInputDirectory('views');
 
-  eleventyConfig.addWatchTarget("./js/");
+  eleventyConfig.addWatchTarget('./js/');
+
+  eleventyConfig.addShortcode('copyright', ()=>`
+    &copy; Copyright 2011 - ${new Date().getFullYear()}
+    <a href="https://danq.me/">Dan Q</a>
+  `);
 };
--- js/fdp.js
+++ js/fdp.js
@@ -1,4 +1,43 @@
-let doc = new jspdf.jsPDF();
-doc.text(30, 30, 'Hello world TEXT');
-const data = doc.output('datauristring');
-document.getElementById('download-link').innerHTML = `<a href="${data}" download>Download PDF</a>`;
+// JS is working:
+document.body.classList.remove('no-js');
+document.body.classList.add('yes-js');
+
+// Form submission:
+document.addEventListener('submit', e=>{
+  if (!e.target.id === 'deed-poll') return;
+  e.preventDefault();
+
+  const lMargin=15; //left margin in mm
+  const rMargin=15; //right margin in mm
+  const pdfInMM=210;  // width of A4 in mm
+
+  const doc = new jspdf.jsPDF();
+
+  doc.text(30, 30, 'Deed of Change of Name');
+
+  const para = `
+    I [OLD NAME] of [ADDRESS] have given up my name [OLD NAME] and have adopted for all purposes
+    the name [NEW NAME].
+
+    Where applicable, I have informed the police that I have given up my name [OLD NAME] and have
+    adopted the name [NEW NAME] and understand that a failure to do so is a criminal offence.
+
+    I also understand that when I apply for official documents in my new name, such as a British
+    Passport, the relevant authorities may check the history and validity of my old name [OLD NAME]
+    and new name [NEW NAME]. They may also check the information in any other supporting documents
+    they have requested.
+
+    Signed as a deed on [date] as [OLD NAME] and [NEW NAME] in the presence of [witness 1 name]
+    of [witness 1 address], and [witness 2 name] of [witness 2 address].
+
+    [your new signature], [your old signature]
+
+    [witness 1 signature], [witness 2 signature]
+  `;
+
+  const lines = doc.splitTextToSize(para, (pdfInMM - lMargin - rMargin));
+	doc.text(lMargin, 20, lines);
+
+  const data = doc.output('datauristring');
+  e.target.innerHTML = `<a href="${data}" download>Download PDF</a>`;
+});
